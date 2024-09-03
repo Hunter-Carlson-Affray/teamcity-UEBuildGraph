8a3e6d1bb1e0cd50a103262724cfbfb47efbc59d
HunterC <hunter.c@affray.gg>
2024-09-02 20:25:25 -0700

    Now Compiles

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-class-properties',
     '@babel/plugin-proposal-export-default-from',
   ],
   env: {
